58741940a23439edc979955adaa1bd4983d03302
Rafael_Estrella <rafaelalfonso82@gmail.com>
2020-06-02 15:38:24 -0400

    Adding animated.css

--- node-firstWebSide/src/index.js
+++ node-firstWebSide/src/index.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 app.use(require('./routes/index'));
 
 //static files
-
+app.use(express.static(path.join(__dirname, '/public')));
 
 //listening the server  
 app.listen(app.get('port'), () => {
